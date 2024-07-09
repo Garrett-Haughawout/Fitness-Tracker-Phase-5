ea2c4a195c21e0f80ac815e23422f43278d8a126
Garhaughawout <108907661+Garhaughawout@users.noreply.github.com>
2024-07-08 22:54:49 -0500

    profile bug fix
    fixed a bug revolving around the friends section on the profile page. This bug caused the friends names to all populate as the name of the user. now the friends names and the corresponding workouts are all returning the correct names.

--- client/src/components/FriendBox.js
+++ client/src/components/FriendBox.js
@@ -5,15 +5,18 @@ import '../styles/FriendBox.css';
 function FriendBox({ friendShip, user }) {
     const [showWorkouts, setShowWorkouts] = useState(false);
 
+    const handleName = friendShip.user_id === user.id ? friendShip.friend.username : friendShip.user.username;
+
+
     return (
         <div className='friendbox-container'>
-            <h3 className='friend-name-header'>{friendShip.friend.username}</h3>
+            <h3 className='friend-name-header'>{handleName}</h3>
             <button className='show-workout-button' onClick={() => setShowWorkouts(!showWorkouts)}>
                 {showWorkouts ? 'Hide Workouts' : 'Show Workouts'}
             </button>
             {showWorkouts ? (
                 <div>
-                    {friendShip.friend.workouts.length > 0 ? friendShip.friend.workouts.map((workout) => (
+                    {friendShip.user.workouts.length > 0 ? friendShip.user.workouts.map((workout) => (
                         <WorkoutBox key={workout.id} workout={workout} user={user} />
                     )) : <p className='no-workouts-message'>No workouts to display</p>}
                 </div>
